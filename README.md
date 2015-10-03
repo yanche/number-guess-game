c32783b429e5491ac500c6c3b6ec1a0205a921c8
yanche <belongs@live.cn>
2015-10-03 02:22:48 -0700

    init

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# number-guess-game
+how to run program,
+1. install nodejs: nodejs.org
+2. command: node index.js 6 144
+NOTE:
+third argument is the number of rounds,
+forth is the result provided by student,
+program will print all possible number combinations.
+
+
+一个教授逻辑学的教授，有三个学生，而且三个学生均非常聪明！
+一天教授给他们出了一个题，教授在每个人脑门上贴了一张纸条并告诉他们，每个人的纸条上都写了一个正整数，且某两个数的和等于第三个！（每个人可以看见另两个数，但看不见自己的） 
+教授问第一个学生：你能猜出自己的数吗？回答：不能
+问第二个，不能
+第三个，不能
+再问第一个，不能
+第二个，不能
+第三个：我猜出来了，是144！
+教授很满意的笑了。
+请问您能猜出另外两个人的数吗？
+
+
+http://www.zhihu.com/question/23999095
---
+++ index.js
@@ -0,0 +1,96 @@
+﻿
+var Knowledge = function () { };
+Knowledge.prototype.utilize = function () {
+    throw new Error('Knowledge:utilize, not implemented');
+};
+
+var GuessKnowledge = function (a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+};
+GuessKnowledge.prototype = Object.create(Knowledge.prototype);
+GuessKnowledge.prototype.utilize = function (user) {
+    switch (user) {
+        case 'A':
+            return new GuessKnowledge(this.b + this.c, this.b, this.c);
+        case 'B':
+            return new GuessKnowledge(this.a, this.a + this.c, this.c);
+        case 'C':
+            return new GuessKnowledge(this.a, this.b, this.a + this.b);
+        default:
+            throw new Error('GuessKnowledge:utilize, unknown user: ' + user);
+    }
+};
+
+var InitKnowledge = function () { };
+InitKnowledge.prototype.utilize = function (user) {
+    switch (user) {
+        case 'A':
+            return new GuessKnowledge(2, 1, 1);
+        case 'B':
+            return new GuessKnowledge(1, 2, 1);
+        case 'C':
+            return new GuessKnowledge(1, 1, 2);
+        default:
+            throw new Error('InitKnowledge:utilize, unknown user: ' + user);
+    }
+};
+
+var User = function (name) {
+    this.name = name;
+    this.knowledge = [new InitKnowledge()];
+};
+User.prototype.guess = function () {
+    var name = this.name;
+    var ret = this.knowledge.map(function (k) { return k.utilize(name); });
+    this.knowledge = [];
+    return ret;
+};
+User.prototype.addKnowledge = function (k) {
+    this.knowledge = this.knowledge.concat(k);
+};
+
+var consAns = function (k, a, name) {
+    switch (name) {
+        case 'A':
+            var q = a / k.a;
+            break;
+        case 'B':
+            var q = a / k.b;
+            break;
+        case 'C':
+            var q = a / k.c;
+            break;
+        default:
+            throw new Error();
+    }
+    var nums = [k.a, k.b, k.c].map(function (e) { return e * q; });
+    if (nums.every(function (n) { return Math.ceil(n) == n; }))
+        return nums;
+    else
+        return null;
+};
+
+var Game = function (round, answer) {
+    this.round = round;
+    this.answer = answer;
+};
+Game.prototype.start = function () {
+    var users = ['A', 'B', 'C'].map(function (name) { return new User(name); });
+    for (var i = 0; i < this.round - 1; ++i) {
+        var u = users[i % 3];
+        var ous = users.filter(function (q) { return q !== u; });
+        var guess = u.guess();
+        ous.forEach(function (q) { q.addKnowledge(guess); });
+    }
+    var fuser = users[(this.round - 1) % 3];
+    var g = fuser.guess();
+    var ans = this.answer, fname = fuser.name;
+    var answers = g.map(function (s) { console.log([s.a, s.b, s.c].join(' ')); return consAns(s, ans, fname); }).filter(function (a) { return a != null; });
+    console.log('Answers: ');
+    answers.forEach(function (a) { console.log(a.join(' ')); });
+};
+
+var round = Number(process.argv[2]), ans = Number(process.argv[3]);
+new Game(round, ans).start();
